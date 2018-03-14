193ae976e90c48b29ef5ccb08148214bc1fedcf3
Kanishk Sharma <kanishksharma3012@gmail.com>
2018-03-14 18:46:08 +0530

    rssi check add

--- examples/lnsm/calculatedist.py
+++ examples/lnsm/calculatedist.py
@@ -4,10 +4,10 @@ import sys
 import math
 import bluetooth
 
-NUM_LOOP = 20
+NUM_LOOP = 30
 
 nearby_devices = bluetooth.discover_devices(
-        duration=8, lookup_names=True, flush_cache=True)
+        duration=10, lookup_names=True, flush_cache=True)
 
 def main():
 
@@ -29,7 +29,10 @@ def main():
         total_dist = 0
         
         for i in range(1, NUM_LOOP):
-            rssi_bt = float(btrssi.get_rssi())
+            rssi = btrssi.get_rssi()
+            if rssi is null:
+                continue    
+            rssi_bt = float(rssi)
             if(rssi_bt!=0 and i>10):                    #reduces initial false values of RSSI using initial delay of 10sec
                 count=count+1
                 x = float((rssi_bt-A0)/(-10*n))         #Log Normal Shadowing Model considering d0 =1m where  
